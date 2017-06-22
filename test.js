dac4809e0ebbec15f0b3fd72c4dae07d2f4c2c57
Jannis R <mail@jannisr.de>
2017-06-23 00:27:05 +0200

    init

---
+++ test.js
@@ -0,0 +1,10 @@
+'use strict'
+
+const test = require('tape')
+const fs = require('fs')
+const path = require('path')
+
+const svg = fs.readFileSync(path.join(__dirname, 'map.svg'), {encoding: 'utf8'})
+
+test('todo', (t) => {
+})
