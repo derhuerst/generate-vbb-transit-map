f91041e8a5a618234c70e17809f2da35ce38184d
Jannis R <mail@jannisr.de>
2017-06-26 18:09:58 +0200

    grouping by line, smoothing

--- index.js
+++ index.js
@@ -2,8 +2,25 @@
 
 const h = require('virtual-hyperscript-svg')
 const color = require('random-color')
+const groupBy = require('lodash.groupby')
 
-const f = (n) => '' + Math.round(n * 1000) / 1000
+const smoothing = require('./lib/smoothing')
+
+const f = (n) => Math.round(n * 1000) / 1000
+
+// const noSmoothing = (polyline) => {
+// 	const first = polyline[0]
+// 	const commands = [
+// 		'M' + f(first[0]) + ' ' + f(first[1])
+// 	]
+
+// 	for (let i = 1; i < polyline.length; i++) {
+// 		const point = polyline[i]
+// 		commands.push('L' + f(point[0]) + ' ' + f(point[1]))
+// 	}
+
+// 	return commands.join(' ')
+// }
 
 const generate = (data) => {
 	// todo: this structure is not valid JGF. change this!
@@ -11,15 +28,25 @@ const generate = (data) => {
 	const {stations, edges, lines, faces} = data
 	const items = []
 
-	for (let edge of edges) {
-		const {from, to, line} = edge
+	const groupedEdges = groupBy(edges, (e) => e.line.id) // todo
+	for (let line in groupedEdges) {
+		const edges = groupedEdges[line]
+		// todo: sort by closest neighbor
+
+		const points = [
+			[f(edges[0].from.coordinate.x), f(edges[0].from.coordinate.y)]
+		]
+		for (let edge of edges) {
+			points.push([f(edge.to.coordinate.x), f(edge.to.coordinate.y)])
+		}
+
+		// const d = noSmoothing(points)
+		const d = smoothing(points)
+
 		items.push(h('path', {
 			class: 'line',
 			style: {stroke: color().hexString()},
-			d: [
-				'M' + f(from.coordinate.x) + ' ' + f(from.coordinate.y),
-				'L' + f(to.coordinate.x) + ' ' + f(to.coordinate.y)
-			].join(' ')
+			d
 		}))
 	}
 
