1ba97bc259eb01aae7aeea7bd369c50e94db6e16
Jannis R <mail@jannisr.de>
2017-06-28 00:31:39 +0200

    revert grouping for now
    as long as the edges aren't sorted, we can't generate bezier curves
    between them.

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 'use strict'
 
 const h = require('virtual-hyperscript-svg')
-const groupBy = require('lodash.groupby')
 const parseLine = require('vbb-parse-line')
 const colors = require('vbb-util/lines/colors')
 
-const smoothing = require('./lib/smoothing')
+// const smoothing = require('./lib/smoothing')
 
 const f = (n) => Math.round(n * 1000) / 1000
 
@@ -27,52 +26,21 @@ const generate = (data) => {
 	const {nodes, edges} = data
 	const items = []
 
-	// for (let edge of edges) {
-	// 	const l = edge.metadata.line
-	// 	const p = parseLine(l).type
-	// 	const c = colors[p] && colors[p][l] && colors[p][l].bg || null
+	for (let edge of edges) {
+		const l = edge.metadata.line
+		const p = parseLine(l).type
+		const c = colors[p] && colors[p][l] && colors[p][l].bg || null
 
-	// 	const from = nodes.find((node) => node.id === edge.from)
-	// 	const to = nodes.find((node) => node.id === edge.to)
+		const from = nodes.find((node) => node.id === edge.from)
+		const to = nodes.find((node) => node.id === edge.to)
 
-	// 	items.push(h('path', {
-	// 		class: 'line',
-	// 		style: {stroke: c || '#777'},
-	// 		d: [
-	// 			'M' + f(from.metadata.coordinates.x) + ' ' + (70 - f(from.metadata.coordinates.y)),
-	// 			'L' + f(to.metadata.coordinates.x) + ' ' + (70 - f(to.metadata.coordinates.y))
-	// 		].join(' ')
-	// 	}))
-	// }
-
-	const groupedEdges = groupBy(edges, (edge) => edge.metadata.line)
-	for (let line in groupedEdges) {
-		const edges = groupedEdges[line]
-		// todo: sort by closest neighbor
-
-		const firstFrom = nodes.find((node) => node.id === edges[0].from)
-		const firstTo = nodes.find((node) => node.id === edges[0].to)
-		const points = [[
-			f(firstFrom.metadata.coordinates.x),
-			f(firstTo.metadata.coordinates.y)
-		]]
-		for (let edge of edges) {
-			const to = nodes.find((node) => node.id === edge.to)
-			points.push([
-				f(to.metadata.coordinates.x),
-				f(to.metadata.coordinates.y)
-			])
-		}
-
-		// const d = noSmoothing(points)
-		const d = smoothing(points)
-
-		const p = parseLine(line).type
-		const c = colors[p] && colors[p][line] && colors[p][line].bg || null
 		items.push(h('path', {
 			class: 'line',
-			style: {stroke: c},
-			d
+			style: {stroke: c || '#777'},
+			d: [
+				'M' + f(from.metadata.coordinates.x) + ' ' + (70 - f(from.metadata.coordinates.y)),
+				'L' + f(to.metadata.coordinates.x) + ' ' + (70 - f(to.metadata.coordinates.y))
+			].join(' ')
 		}))
 	}
 
